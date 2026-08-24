b785d2d0187e9eba95277b714ba0431d2980588c
kabakcibt <bilallyedtk@gmail.com>
2026-08-24 15:05:20 +0300

    Gün 4 tamamlandı: MSSQL veritabanı, tablolar ve Python pyodbc bağlantısı eklendi.

---
+++ Project1_SecureWeb/db.py
@@ -0,0 +1,27 @@
+import pyodbc
+
+def get_db_connection():
+    server = 'localhost\\SQLEXPRESS'  # Veya sadece 'localhost' eğer named instance değilse
+    database = 'SecureTaskManagerDB'
+
+    connection_string = (
+        f"DRIVER={{ODBC Driver 17 for SQL Server}};"
+        f"SERVER={server};"
+        f"DATABASE={database};"
+        f"Trusted_Connection=yes;"
+    )
+
+    try:
+        conn = pyodbc.connect(connection_string)
+        return conn
+    except Exception as e:
+        print(f"Veritabani baglanti hatasi: {e}")
+        return None
+
+if __name__ == "__main__":
+    conn = get_db_connection()
+    if conn:
+        print("Tebrikler ! MSSQL veritabani baglantisi basariyla saglandi.")
+        conn.close()
+    else:
+        print("Baglanti kurulamadi!")
